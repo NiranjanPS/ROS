2b92dd1867ffc693e5bd322d464890d6fac99df0
NIRANJAN P SALIMATH <niranjanps073@gmail.com>
2019-09-15 23:09:51 +0530

    Add files via upload

---
+++ ROS_implementation_Python/Data_Visualization/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/n/catkin_ws_1/src
+# Build directory: /home/n/catkin_ws_1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(data_viz)
